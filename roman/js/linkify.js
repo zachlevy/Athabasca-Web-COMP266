52ce9f97a1181afd6c3d7568b2a095e2521dec06
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-07-23 13:58:46 -0400

    added linkify

---
+++ roman/js/linkify.js
@@ -0,0 +1,38 @@
+/*
+source: http://jsfiddle.net/hakim/Ht6Ym/
+author: hakim
+date: 20140723
+*/
+// on window load
+window.onload=function(){
+	// set true if the browser supports the perspective alteration
+	var supports3DTransforms =  document.body.style['webkitPerspective'] !== undefined || document.body.style['MozPerspective'] !== undefined;
+	// function to transform elements
+	function linkify( selector ) {
+		// if browser supports transform, else print failure to console
+	    if( supports3DTransforms ) {
+	        // get all elements that match the selector
+	        var nodes = document.querySelectorAll( selector );
+	        // for each element
+	        for( var i = 0, len = nodes.length; i < len; i++ ) {
+	            var node = nodes[i];
+	            // if the class doesn't already roll
+	            if( !node.className || !node.className.match( /roll/g ) ) {
+	            	// roll
+	                node.className += ' roll';
+	                /*
+	                uncomment next line to roll all elements that match selector, otherwise you'll have to wrap text with
+	                <span data-title="roll text"></span>
+	                */
+	                //node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
+	            }
+	        };
+	    } else {
+	    	console.log ("failture, browser doesn't support 3d");
+	    }
+	}
+	// set which html elements you want to have scroll effect
+	linkify( 'h1' );
+	linkify( 'h2' );
+	linkify( 'h3' );
+}
